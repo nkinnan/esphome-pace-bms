4a42d2c98c6178b6465a2a865bd47bc28b497ff0
unknown <nick@nickkinnan.com>
2024-08-22 00:48:32 -0700

    esp skeleton

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.cpp_helpers import gpio_pin_expression
 from esphome.components import sensor, uart
 from esphome.const import (
     CONF_FLOW_CONTROL_PIN,
