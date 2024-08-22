3e84411fa985cb34021d4aa6fcb76c2338162c11
unknown <nick@nickkinnan.com>
2024-08-22 13:50:36 -0700

    esp skeleton

--- components/pace_bms/pace_bms.cpp
+++ components/pace_bms/pace_bms.cpp
@@ -115,9 +115,12 @@ void PaceBms::parse_data_frame_(uint8_t* frame_bytes, uint8_t frame_length) {
 }
 
 void PaceBms::dump_config() {
-  ESP_LOGCONFIG(TAG, "pace_bms:");
-  LOG_PIN(           "  Flow Control Pin: ", this->flow_control_pin_);
-  ESP_LOGCONFIG(TAG, "  Address: ", this->address_);
+  ESP_LOGCONFIG(TAG,   "pace_bms:");
+  if(this->flow_control_pin_ != nullptr)
+    LOG_PIN(           "  Flow Control Pin: ", this->flow_control_pin_);
+  else
+    ESP_LOGCONFIG(TAG, "  Flow Control Pin: N/A");
+  ESP_LOGCONFIG(TAG,   "  Address: ", this->address_);
   //LOG_SENSOR("  ", "Voltage", this->voltage_sensor_);
   this->check_uart_settings(9600);
 }
