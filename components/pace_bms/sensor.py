e68ad7e8be210a685040b68a0de0a950a008d616
unknown <nick@nickkinnan.com>
2024-08-22 00:29:48 -0700

    esp skeleton

--- components/pace_bms/sensor.py
+++ components/pace_bms/sensor.py
@@ -91,6 +91,7 @@ FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
 
 
 async def to_code(config):
+    cg.add_global(pace_bms_ns.using)
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
