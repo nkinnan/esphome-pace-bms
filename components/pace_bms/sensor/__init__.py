5e65d4e01a09bcc83c6d394f9eeb122cb8c9de86
unknown <nick@nickkinnan.com>
2024-08-22 16:33:36 -0700

    esp skeleton

--- components/pace_bms/sensor/__init__.py
+++ components/pace_bms/sensor/__init__.py
@@ -19,11 +19,11 @@ DEPENDENCIES = ["pace_bms"]
 
 PaceBmsSensor = pace_bms_ns.class_("PaceBmsSensor", cg.PollingComponent)
 
-CONF_PACE_BMS_SENSOR_ID = "pace_bms_sensor_id"
+#CONF_PACE_BMS_SENSOR_ID = "pace_bms_sensor_id"
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_PACE_BMS_SENSOR_ID): cv.declare_id(PaceBmsSensor),
+        cv.GenerateID(): cv.declare_id(PaceBmsSensor),
         cv.GenerateID(CONF_PACE_BMS_ID): cv.use_id(PaceBms),
         cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
             unit_of_measurement=UNIT_VOLT,
