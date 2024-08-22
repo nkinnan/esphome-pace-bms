697b49cc30857e3e5ab634c789d04850076a1099
unknown <nick@nickkinnan.com>
2024-08-22 02:32:41 -0700

    esp skeleton

--- components/pace_bms/pace_bms.h
+++ components/pace_bms/pace_bms.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
+#include "sensor/sensor.h"
 //#include "esphome/components/binary_sensor/binary_sensor.h"
 //#include "esphome/components/text_sensor/text_sensor.h"
 
