bfbc6dba43eb805b6b484631cd20914c2c5332ec
unknown <nick@nickkinnan.com>
2024-08-22 22:39:59 -0700

    esp skeleton

--- components/pace_bms/sensor/pace_bms_sensor.cpp
+++ components/pace_bms/sensor/pace_bms_sensor.cpp
@@ -20,7 +20,7 @@ void PaceBmsSensor::dump_config() {
 	//LOG_SENSOR("", "Sun Sensor", this); 
 	ESP_LOGCONFIG(TAG, "pace_bms_sensor:");
 	LOG_SENSOR("  ", "Cell Count", this->cell_count_sensor_);
-	for (int i = 1; i <= 16); i++) {
+	for (int i = 1; i <= 16; i++) {
 	  char name[16];
 	  sprintf(name, "Cell Voltage %02i", i);
 	  LOG_SENSOR("  ", name, this->cell_voltage_sensor_[i]);
