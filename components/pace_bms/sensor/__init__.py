5b5fa6964ad1ac10619fd44d9ec3044a229a173e
unknown <nick@nickkinnan.com>
2024-08-22 18:32:29 -0700

    esp skeleton

--- components/pace_bms/sensor/__init__.py
+++ components/pace_bms/sensor/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
+    CONF_ID,
     CONF_VOLTAGE,
     UNIT_VOLT,
     DEVICE_CLASS_VOLTAGE,
@@ -13,11 +14,11 @@ DEPENDENCIES = ["pace_bms"]
 
 PaceBmsSensor = pace_bms_ns.class_("PaceBmsSensor", cg.Component)
 
-CONF_PACE_BMS_SENSOR_ID = "pace_bms_sensor_id"
+#CONF_PACE_BMS_SENSOR_ID = "pace_bms_sensor_id"
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_PACE_BMS_SENSOR_ID): cv.declare_id(PaceBmsSensor),
+        cv.GenerateID(CONF_ID): cv.declare_id(PaceBmsSensor),
         cv.GenerateID(CONF_PACE_BMS_ID): cv.use_id(PaceBms),
         cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
             unit_of_measurement=UNIT_VOLT,
@@ -30,7 +31,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_PACE_BMS_SENSOR_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     paren = await cg.get_variable(config[CONF_PACE_BMS_ID])
