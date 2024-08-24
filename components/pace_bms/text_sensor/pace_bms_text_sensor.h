b306479d107d9cf2e4423af771b0ff7bfabcb138
unknown <nick@nickkinnan.com>
2024-08-23 17:57:36 -0700

    esp skeleton

--- components/pace_bms/text_sensor/pace_bms_text_sensor.h
+++ components/pace_bms/text_sensor/pace_bms_text_sensor.h
@@ -12,12 +12,12 @@ namespace pace_bms {
 class PaceBmsTextSensor : public Component {
  public:
   void set_parent(PaceBms *parent) { parent_ = parent; }
-  void set_warning_status_sensor(sensor::Sensor* warning_status_sensor) { warning_status_sensor_ = warning_status_sensor; request_status_info_callback_ = true; }
-  void set_balancing_status_sensor(sensor::Sensor* balancing_status_sensor) { balancing_status_sensor_ = balancing_status_sensor; request_status_info_callback_ = true; }
-  void set_system_status_sensor(sensor::Sensor* system_status_sensor) { system_status_sensor_ = system_status_sensor; request_status_info_callback_ = true; }
-  void set_configuration_status_sensor(sensor::Sensor* configuration_status_sensor) { configuration_status_sensor_ = configuration_status_sensor; request_status_info_callback_ = true; }
-  void set_protection_status_sensor(sensor::Sensor* protection_status_sensor) { protection_status_sensor_ = protection_status_sensor; request_status_info_callback_ = true; }
-  void set_fault_status_sensor(sensor::Sensor* fault_status_sensor) { fault_status_sensor_ = fault_status_sensor; request_status_info_callback_ = true; }
+  void set_warning_status_sensor(text_sensor::TextSensor* warning_status_sensor) { warning_status_sensor_ = warning_status_sensor; request_status_info_callback_ = true; }
+  void set_balancing_status_sensor(text_sensor::TextSensor* balancing_status_sensor) { balancing_status_sensor_ = balancing_status_sensor; request_status_info_callback_ = true; }
+  void set_system_status_sensor(text_sensor::TextSensor* system_status_sensor) { system_status_sensor_ = system_status_sensor; request_status_info_callback_ = true; }
+  void set_configuration_status_sensor(text_sensor::TextSensor* configuration_status_sensor) { configuration_status_sensor_ = configuration_status_sensor; request_status_info_callback_ = true; }
+  void set_protection_status_sensor(text_sensor::TextSensor* protection_status_sensor) { protection_status_sensor_ = protection_status_sensor; request_status_info_callback_ = true; }
+  void set_fault_status_sensor(text_sensor::TextSensor* fault_status_sensor) { fault_status_sensor_ = fault_status_sensor; request_status_info_callback_ = true; }
 
   void setup() override;
   float get_setup_priority() const override;
