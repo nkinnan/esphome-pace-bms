6fc9a087014063e508ac1617d52ba2313a5907da
unknown <nick@nickkinnan.com>
2024-08-24 12:24:06 -0700

    esp skeleton

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -19,9 +19,13 @@ PaceBms = pace_bms_ns.class_("PaceBms", cg.PollingComponent, uart.UARTDevice)
 CONF_PACE_BMS_ID = "pace_bms_id"
 
 CONF_PROTOCOL_VERSION = "protocol_version"
+CONF_REQUEST_THROTTLE = "request_throttle"
+CONF_RESPONSE_TIMEOUT = "response_timeout"
 
 DEFAULT_ADDRESS = 0
 DEFAULT_PROTOCOL_VERSION = 0x25
+DEFAULT_REQUEST_THROTTLE = "50ms"
+DEFAULT_RESPONSE_TIMEOUT = "200ms"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -30,9 +34,11 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_FLOW_CONTROL_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_ADDRESS, default=DEFAULT_ADDRESS): cv.int_range(min=0, max=15),
             cv.Optional(CONF_PROTOCOL_VERSION, default=DEFAULT_PROTOCOL_VERSION): cv.int_range(min=0x25, max=0x25),
+            cv.Optional(CONF_REQUEST_THROTTLE, default=DEFAULT_REQUEST_THROTTLE): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_RESPONSE_TIMEOUT, default=DEFAULT_RESPONSE_TIMEOUT): cv.positive_time_period_milliseconds,
         }
     )
-    .extend(cv.polling_component_schema("5s"))
+    .extend(cv.polling_component_schema("60s"))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
@@ -53,3 +59,8 @@ async def to_code(config):
         cg.add(var.set_address(config[CONF_ADDRESS]))
     if CONF_PROTOCOL_VERSION in config:
         cg.add(var.set_protocol_version(config[CONF_PROTOCOL_VERSION]))
+    if CONF_COMMAND_THROTTLE in config:
+        cg.add(var.set_request_throttle(config[CONF_REQUEST_THROTTLE]))
+    if CONF_RESPONSE_TIMEOUT in config:
+        cg.add(var.set_response_timeout(config[CONF_RESPONSE_TIMEOUT]))
+
