b03b109e14204f90145bfdfc0b7e018df7d95b18
unknown <nick@nickkinnan.com>
2024-08-24 18:08:05 -0700

    esp skeleton

--- components/pace_bms/switch_implementation/__init__.py
+++ components/pace_bms/switch_implementation/__init__.py
@@ -21,6 +21,8 @@ CONFIG_SCHEMA = cv.All(
     )
 )
 
+FINAL_VALIDATE_SCHEMA = CONFIG_SCHEMA
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
