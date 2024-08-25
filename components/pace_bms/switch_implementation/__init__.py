9da76721f64693b6f23be2a626868dfbdf711e47
unknown <nick@nickkinnan.com>
2024-08-24 18:20:39 -0700

    esp skeleton

--- components/pace_bms/switch_implementation/__init__.py
+++ components/pace_bms/switch_implementation/__init__.py
@@ -11,14 +11,18 @@ from .. import pace_bms_ns
 
 PaceBmsSwitchImplementation = pace_bms_ns.class_("PaceBmsSwitchImplementation", cg.Component, switch.Switch)
 
-CONFIG_SCHEMA = cv.All(
-    switch.switch_schema(default_restore_mode="DISABLED")
-    .extend(
+CONFIG_SCHEMA = (
+    cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(PaceBmsSwitchImplementation),
-            #cv.GenerateID(CONF_PACE_BMS_SWITCH_ID): cv.use_id(PaceBmsSwitch),
+            cv.Optional(CONF_FLOW_CONTROL_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_ADDRESS, default=DEFAULT_ADDRESS): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_PROTOCOL_VERSION, default=DEFAULT_PROTOCOL_VERSION): cv.int_range(min=0x25, max=0x25),
+            cv.Optional(CONF_REQUEST_THROTTLE, default=DEFAULT_REQUEST_THROTTLE): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_RESPONSE_TIMEOUT, default=DEFAULT_RESPONSE_TIMEOUT): cv.positive_time_period_milliseconds,
         }
     )
+    .extend(switch.switch_schema(default_restore_mode="DISABLED"))
 )
 
 #FINAL_VALIDATE_SCHEMA = CONFIG_SCHEMA
