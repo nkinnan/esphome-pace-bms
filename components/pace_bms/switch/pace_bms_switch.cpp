19a7bd81e08c144a4bb3b2fbdb6e1e17b4e38f57
unknown <nick@nickkinnan.com>
2024-08-25 10:39:56 -0700

    esp skeleton

--- components/pace_bms/switch/pace_bms_switch.cpp
+++ components/pace_bms/switch/pace_bms_switch.cpp
@@ -6,7 +6,7 @@
 namespace esphome {
 namespace pace_bms {
 
-static const char* const TAG = "pace_bms.sensor";
+static const char* const TAG = "pace_bms.switch";
 
 void PaceBmsSwitch::setup() {
   if (this->request_status_information_callback_ == true) {
@@ -16,7 +16,8 @@ void PaceBmsSwitch::setup() {
 	this->buzzer_switch_->add_on_write_state_callback([this](bool state) {
 	  this->parent_->set_switch_state(PaceBms::ST_BuzzerAlarm, state);
 	  // set internal (requested) state but do not save/publish yet until the device confirms
-	  this->buzzer_switch_->publish_state(state);
+	  ESP_LOGE(TAG, "Setting internal switch state without publish due to write state change request: %s", state ? "true" : "false");
+	  this->buzzer_switch_->record_state_without_publish(state);
 	});
   }
 }
@@ -30,6 +31,7 @@ void PaceBmsSwitch::dump_config() {
 
 void PaceBmsSwitch::status_information_callback(PaceBmsV25::StatusInformation& status_information) {
   if (this->buzzer_switch_ != nullptr) {
+	  ESP_LOGE(TAG, "Publishing state due to update from the hardware: %s", (status_information.configuration_value & PaceBmsV25::CF_WarningBuzzerEnabledBit) != 0 ? "true" : "false");
 	// unlike record_state_without_publish, upon confirmation from the device that the state was actually set, do a full save/publish
     this->buzzer_switch_->publish_state((status_information.configuration_value & PaceBmsV25::CF_WarningBuzzerEnabledBit) != 0);
   }
