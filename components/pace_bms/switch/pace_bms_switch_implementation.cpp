754291e39785da2191ee2bc14e670396e72f04ca
unknown <nick@nickkinnan.com>
2024-08-24 22:34:15 -0700

    esp skeleton

--- components/pace_bms/switch/pace_bms_switch_implementation.cpp
+++ components/pace_bms/switch/pace_bms_switch_implementation.cpp
@@ -3,9 +3,12 @@
 namespace esphome {
 namespace pace_bms {
 
+static const char* const TAG = "pace_bms.sensor_implementation";
+
 // the only purpose of this class is to fill in the pure virtual and call the parent container component on state change
 void PaceBmsSwitchImplementation::write_state(bool state) {
 	//this->parent_->child_switch_state_changed(this, state);
+	ESP_LOGV(TAG, "New state: %b", state);
 }
 
 void PaceBmsSwitchImplementation::setup() {
@@ -14,6 +17,7 @@ void PaceBmsSwitchImplementation::setup() {
 float PaceBmsSwitchImplementation::get_setup_priority() const { return setup_priority::DATA; }
 
 void PaceBmsSwitchImplementation::dump_config() {
+	ESP_LOGCONFIG(TAG, "pace_bms_switch_implementation:");
 }
 
 }  // namespace pace_bms
