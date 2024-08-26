6fc039355e24705106883048bbebda5123ceb8b6
unknown <nick@nickkinnan.com>
2024-08-25 23:07:29 -0700

    esp skeleton

--- components/pace_bms/select/pace_bms_select.cpp
+++ components/pace_bms/select/pace_bms_select.cpp
@@ -16,8 +16,10 @@ void PaceBmsSelect::setup() {
 	this->charge_current_limiter_gear_select_->add_on_control_callback([this](std::string state) {
 	  if(state == "LowGear")
 	    this->parent_->set_charge_current_limiter_gear(PaceBms::CLG_LowGear);
-	  if(state == "HighGear")
+	  else if(state == "HighGear")
 	    this->parent_->set_charge_current_limiter_gear(PaceBms::CLG_HighGear);
+	  else
+		ESP_LOGE("Select option state not understood");
 	});
   }
 }
@@ -32,7 +34,7 @@ void PaceBmsSelect::dump_config() {
 void PaceBmsSelect::status_information_callback(PaceBmsV25::StatusInformation& status_information) {
   if (this->charge_current_limiter_gear_select_ != nullptr) {
 	std::string state = (status_information.configuration_value & PaceBmsV25::CF_ChargeCurrentLimiterLowGearSetBit ? "LowGear" : "HighGear");
-	ESP_LOGV(TAG, "'charge_current_limiter_gear': Publishing state due to update from the hardware: %s", state);
+	ESP_LOGV(TAG, "'charge_current_limiter_gear': Publishing state due to update from the hardware: %s", state.c_str());
     this->charge_current_limiter_gear_select_->publish_state(state);
   }
 }
