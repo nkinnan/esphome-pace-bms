c1d64127b5f61fbeb76e89277239cace8e6027a4
unknown <nick@nickkinnan.com>
2024-08-25 19:40:28 -0700

    esp skeleton

--- components/pace_bms/select/pace_bms_select_implementation.h
+++ components/pace_bms/select/pace_bms_select_implementation.h
@@ -10,7 +10,7 @@ class PaceBmsSelectImplementation : public Component, public select::Select {
  public:
   float get_setup_priority() const override;
 
-  void add_on_control_callback(std::function<void(const std::string)>&& callback);
+  void add_on_control_callback(std::function<void(const std::string&)>&& callback);
 
  protected:
   // the only purpose of this class is to simply fill in this pure virtual and call the parent container component on user initiated state change request
