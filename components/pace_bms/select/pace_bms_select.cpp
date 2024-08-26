d5d7d38ac519fac7cf7d6208cd602f4a240235c5
unknown <nick@nickkinnan.com>
2024-08-25 23:02:05 -0700

    esp skeleton

--- components/pace_bms/select/pace_bms_select.cpp
+++ components/pace_bms/select/pace_bms_select.cpp
@@ -14,10 +14,9 @@ void PaceBmsSelect::setup() {
   }
   if (this->charge_current_limiter_gear_select_ != nullptr) {
 	this->charge_current_limiter_gear_select_->add_on_control_callback([this](std::string state) {
-      ESP_LOGV("Select set: %s", state.c_str());
 	  if(state == "LowGear")
 	    this->parent_->set_charge_current_limiter_gear(PaceBms::CLG_LowGear);
-	  else
+	  if(state == "HighGear")
 	    this->parent_->set_charge_current_limiter_gear(PaceBms::CLG_HighGear);
 	});
   }
