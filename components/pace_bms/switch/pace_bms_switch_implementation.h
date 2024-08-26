09a59758480ebdbed26ca37a58c3f6668f50848a
unknown <nick@nickkinnan.com>
2024-08-25 19:07:06 -0700

    esp skeleton

--- components/pace_bms/switch/pace_bms_switch_implementation.h
+++ components/pace_bms/switch/pace_bms_switch_implementation.h
@@ -10,13 +10,13 @@ class PaceBmsSwitchImplementation : public Component, public switch_::Switch {
  public:
   float get_setup_priority() const override;
 
-  void add_on_control_callback(std::function<void(bool)>&& callback);
+  void add_on_write_state_callback(std::function<void(bool)>&& callback);
 
  protected:
   // the only purpose of this class is to simply fill in this pure virtual and call the parent container component on user initiated state change request
-  void control(bool state) override;
+  void write_state(bool state) override;
 
-  CallbackManager<void(bool)> control_callback_{};
+  CallbackManager<void(bool)> write_state_callback_{};
 };
 
 }  // namespace pace_bms
