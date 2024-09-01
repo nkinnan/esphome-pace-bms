b96c74655e193a391f89bd130276acd316215b42
unknown <nick@nickkinnan.com>
2024-09-01 03:13:47 -0700

    esp skeleton

--- components/pace_bms/select/pace_bms_select_implementation.cpp
+++ components/pace_bms/select/pace_bms_select_implementation.cpp
@@ -23,10 +23,14 @@ uint8_t PaceBmsSelectImplementation::value_from_option(std::string text)
 {
 	auto options = this->traits.get_options();
 	auto opt_it = std::find(options.cbegin(), options.cend(), text);
+	if (opt_it == options.cend()) {
+		ESP_LOGD(TAG, "%s: unable to find value for option '%s'", this->name_.c_str(), text.c_str());
+		return 0xFF;
+	}
 	size_t idx = std::distance(options.cbegin(), opt_it);
 	uint8_t value = this->values_[idx];
 
-	ESP_LOGD(TAG, "%s found value 0x%02X for option '%s'", this->name_.c_str(), value, text.c_str());
+	ESP_LOGD(TAG, "%s: found value 0x%02X for option '%s'", this->name_.c_str(), value, text.c_str());
 
 	return value;
 }
@@ -34,10 +38,14 @@ uint8_t PaceBmsSelectImplementation::value_from_option(std::string text)
 std::string PaceBmsSelectImplementation::option_from_value(uint8_t value) {
 	auto options = this->traits.get_options();
 	auto opt_it = std::find(this->values_.cbegin(), this->values_.cend(), value);
+	if (opt_it == this->values_.cend()) {
+		ESP_LOGD(TAG, "%s: unable to find option for value 0x%02X", this->name_.c_str(), value);
+		return std::string("error");
+	}
 	size_t idx = std::distance(this->values_.cbegin(), opt_it);
 	std::string text = std::string(options[idx]);
 
-	ESP_LOGD(TAG, "%s found option '%s' for value 0x%02X", this->name_.c_str(), text.c_str(), value);
+	ESP_LOGD(TAG, "%s: found option '%s' for value 0x%02X", this->name_.c_str(), text.c_str(), value);
 
 	return text;
 }
