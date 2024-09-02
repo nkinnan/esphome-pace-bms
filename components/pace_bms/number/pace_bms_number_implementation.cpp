a0fc1c1af4379b9480783a12457a3c343e2254e2
unknown <nick@nickkinnan.com>
2024-09-01 19:34:00 -0700

    esp skeleton

--- components/pace_bms/number/pace_bms_number_implementation.cpp
+++ components/pace_bms/number/pace_bms_number_implementation.cpp
@@ -10,7 +10,7 @@ void PaceBmsNumberImplementation::control(float number) {
 	ESP_LOGE(TAG, "Publishing new value %f", number);
 
 	// required for the UX not to get out of sync
-	this->publish_state(state);
+	this->publish_state(number);
 }
 
 float PaceBmsNumberImplementation::get_setup_priority() const { return setup_priority::DATA; }
