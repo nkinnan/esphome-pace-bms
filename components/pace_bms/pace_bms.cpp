cc3928dbeee87f85bbd6e79bc17bbb5e606bcef1
unknown <nick@nickkinnan.com>
2024-09-02 11:02:50 -0700

    esp skeleton

--- components/pace_bms/pace_bms.cpp
+++ components/pace_bms/pace_bms.cpp
@@ -506,7 +506,7 @@ void PaceBms::handle_write_configuration_response(std::vector<uint8_t>& response
 
 void PaceBms::write_queue_push_back_with_deduplication(command_item* item) {
     auto iter = std::find(this->write_queue_.begin(), this->write_queue_.end(), 
-        [item](const command_item* test) bool -> {
+        [item](const command_item* test) -> bool {
             return (*test) == (*item);
         });
 
