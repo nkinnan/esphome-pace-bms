564cb1464edf55cb085caff3f803cb5ebe983fd6
unknown <nick@nickkinnan.com>
2024-09-08 20:06:01 -0700

    everything except the two mystery commands

--- components/pace_bms/datetime/__init__.py
+++ components/pace_bms/datetime/__init__.py
@@ -25,7 +25,7 @@ CONFIG_SCHEMA = cv.Schema(
 
         cv.Optional(CONF_SYSTEM_DATE_AND_TIME): datetime.datetime_schema(
             PaceBmsDatetimeImplementation,
-            requires_time = false,
+            requires_time = False,
         ).extend({ 
             cv.Optional(CONF_TYPE, default="DATETIME"): cv.one_of("DATETIME", upper=True),
         }),
@@ -41,7 +41,7 @@ async def to_code(config):
     cg.add(var.set_parent(paren))
 
     if system_date_and_time_config := config.get(CONF_SYSTEM_DATE_AND_TIME):
-        dt = await datetime.new_datetime(system_date_and_time_config)
+        dt = await datetime.new_datetime(system_date_and_time_config, requires_time = False)
         cg.add(var.set_system_date_and_time_datetime(dt))
 
 
