deaca31e0403e894a03e6addcfaa7a1aea766344
unknown <nick@nickkinnan.com>
2024-09-24 23:18:24 -0700

    typo

--- components/pace_bms/number/pace_bms_number.h
+++ components/pace_bms/number/pace_bms_number.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/pace_bms/pace_bms.h"
+#include "esphome/components/pace_bms/pace_bms_component.h"
 
 #include "pace_bms_number_implementation.h"
 
