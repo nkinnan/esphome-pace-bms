14c98e225e5288d2f06065d3a48d5f94486b9503
Nick Kinnan <nick@nickkinnan.com>
2025-09-28 21:18:21 -0700

    Update pace_bms_protocol_v25.cpp

--- components/pace_bms/pace_bms_protocol_v25.cpp
+++ components/pace_bms/pace_bms_protocol_v25.cpp
@@ -123,7 +123,7 @@ bool PaceBmsProtocolV25::ProcessReadAnalogInformationResponse(const uint8_t busI
 		}
 	}
 #if ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERY_VERBOSE
-	ESP_LOGVV(TAG, std::to_string(responseCount) + " responses found in payload");
+	LogVeryVerbose(std::to_string(responseCount) + " responses found in payload");
 #endif
 
 	for(int i = 0; i < responseCount; i++)
