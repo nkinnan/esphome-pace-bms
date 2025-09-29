235746b8d5fea34aa0ef7658203ec901ca84d269
Nick Kinnan <nick@nickkinnan.com>
2025-09-29 07:08:58 -0700

    Update pace_bms_component.cpp

--- components/pace_bms/pace_bms_component.cpp
+++ components/pace_bms/pace_bms_component.cpp
@@ -97,14 +97,14 @@ void PaceBms::setup() {
 		// asking for analog info is always the first thing (here and in update()) so that we can sniff the User Defined Value field to determine the protocol variant
 		if(this->slave_discovery_mode_ == SLAVE_DISCOVERY_MODE_BROADCAST || this->slave_discovery_mode_ == SLAVE_DISCOVERY_MODE_RELAY_AND_BROADCAST) {
 			command_item* item = new command_item;
-			item->description_ = std::string("slave discovery: broadcast query for analog information");
+			item->description_ = std::string("slave discovery broadcast: query for analog information");
 			item->create_request_frame_ = [this](std::vector<uint8_t>& request) -> bool { return this->pace_bms_v25_->CreateReadAnalogInformationRequest(0xFF, request); };
 			item->process_response_frame_ = [this](std::vector<uint8_t>& response) -> void { this->handle_slave_discovery_broadcast_read_analog_information_response_v25(response); };
 			read_queue_.push(item);
 		}
 		if(this->slave_discovery_mode_ == SLAVE_DISCOVERY_MODE_BROADCAST || this->slave_discovery_mode_ == SLAVE_DISCOVERY_MODE_RELAY_AND_BROADCAST) {
 			command_item* item = new command_item;
-			item->description_ = std::string("slave discovery: broadcast query for status information");
+			item->description_ = std::string("slave discovery broadcast: query for status information");
 			item->create_request_frame_ = [this](std::vector<uint8_t>& request) -> bool { return this->pace_bms_v25_->CreateReadStatusInformationRequest(0xFF, request); };
 			item->process_response_frame_ = [this](std::vector<uint8_t>& response) -> void { this->handle_slave_discovery_broadcast_read_status_information_response_v25(response); };
 			read_queue_.push(item);
@@ -115,7 +115,7 @@ void PaceBms::setup() {
 				if(address == this->address_) 
 					continue;
 				command_item* item = new command_item;
-				item->description_ = std::string("slave discovery: query slave address " + std::to_string(address) + " for analog information");
+				item->description_ = std::string("slave discovery relay: query slave address " + std::to_string(address) + " for analog information");
 				item->create_request_frame_ = [this, address](std::vector<uint8_t>& request) -> bool { return this->pace_bms_v25_->CreateReadAnalogInformationRequest(address, request); };
 				item->process_response_frame_ = [this, address](std::vector<uint8_t>& response) -> void { this->handle_slave_discovery_relay_read_analog_information_response_v25(address, response); };
 				read_queue_.push(item);
@@ -127,7 +127,7 @@ void PaceBms::setup() {
 				if(address == this->address_) 
 					continue;
 				command_item* item = new command_item;
-				item->description_ = std::string("slave discovery: query slave address " + std::to_string(address) + " for status information");
+				item->description_ = std::string("slave discovery relay: query slave address " + std::to_string(address) + " for status information");
 				item->create_request_frame_ = [this, address](std::vector<uint8_t>& request) -> bool { return this->pace_bms_v25_->CreateReadStatusInformationRequest(address, request); };
 				item->process_response_frame_ = [this, address](std::vector<uint8_t>& response) -> void { this->handle_slave_discovery_relay_read_status_information_response_v25(address, response); };
 				read_queue_.push(item);
