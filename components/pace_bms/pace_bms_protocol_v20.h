8623bfe216634367ee9640b1afb519e980b53b08
Nick Kinnan <nick@nickkinnan.com>
2025-10-03 03:53:07 -0700

    Update pace_bms_protocol_v20.h

--- components/pace_bms/pace_bms_protocol_v20.h
+++ components/pace_bms/pace_bms_protocol_v20.h
@@ -107,7 +107,7 @@ public:
 	};
 
 	bool CreateReadAnalogInformationRequest(const uint8_t busId, std::vector<uint8_t>& request);
-	bool ProcessReadAnalogInformationResponse(const uint8_t busId, this->address_, std::optional<uint8_t> respondingBusId, const std::span<uint8_t>& response, AnalogInformation& analogInformation);
+	bool ProcessReadAnalogInformationResponse(const uint8_t busId, const uint8_t targetedBusId, std::optional<uint8_t> respondingBusId, const std::span<uint8_t>& response, AnalogInformation& analogInformation);
 
 protected:
 	// protocol variants
