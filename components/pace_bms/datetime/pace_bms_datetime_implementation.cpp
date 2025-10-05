4d015fb0fdbc6685280369e2b993045a764b896a
Nick Kinnan <nick@nickkinnan.com>
2025-10-05 14:42:04 -0700

    Update pace_bms_datetime_implementation.cpp

--- components/pace_bms/datetime/pace_bms_datetime_implementation.cpp
+++ components/pace_bms/datetime/pace_bms_datetime_implementation.cpp
@@ -22,7 +22,7 @@ void PaceBmsDatetimeImplementation::set_datetime(PaceBmsProtocolV25::DateTime& d
 }
 
 void PaceBmsDatetimeImplementation::control(const datetime::DateTimeCall& call) {
-	ESP_LOGD(TAG, "control: new value %04i:%02i:%02i %02i:%02i:%02i", call.get_year(), call.get_month(), call.get_day(), call.get_hour(), call.get_minute(), call.get_second());
+	ESP_LOGD(TAG, "control: new value %04hu:%02hu:%02hu %02hu:%02hu:%02hu", call.get_year(), call.get_month(), call.get_day(), call.get_hour(), call.get_minute(), call.get_second());
 
 	// callbacks
 	this->control_callbacks_.call(call);
