59976fb7a8946dc807531a83b14331d0bdd9e1c5
Nick Kinnan <nick@nickkinnan.com>
2025-10-05 21:42:32 -0700

    ghsd

--- components/pace_bms/pace_bms_component_slave.cpp
+++ components/pace_bms/pace_bms_component_slave.cpp
@@ -48,8 +48,8 @@ void PaceBmsSlave::queue_sensor_update(std::function<void()> update) { this->par
 //     periodically gather these updates for fan-out to the sensors the first place
 void PaceBmsSlave::register_bms_count_callback_v25(std::function<void(uint8_t&)> callback) { ESP_LOGE(TAG, "non-analog/status information commands not available for slave devices"); }
 void PaceBmsSlave::register_payload_count_callback_v25(std::function<void(uint8_t&)> callback) { ESP_LOGE(TAG, "non-analog/status information commands not available for slave devices"); }
-void PaceBmsSlave::register_analog_information_callback_v25(std::function<void(PaceBmsProtocolV25::AnalogInformation&)> callback) { this->register_analog_information_callback_v25(callback); }
-void PaceBmsSlave::register_status_information_callback_v25(std::function<void(PaceBmsProtocolV25::StatusInformation&)> callback) { this->register_status_information_callback_v25(callback); }
+void PaceBmsSlave::register_analog_information_callback_v25(std::function<void(PaceBmsProtocolV25::AnalogInformation&)> callback) { analog_information_callbacks_v25_.push_back(std::move(callback)); }
+void PaceBmsSlave::register_status_information_callback_v25(std::function<void(PaceBmsProtocolV25::StatusInformation&)> callback) { status_information_callbacks_v25_.push_back(std::move(callback)); }
 void PaceBmsSlave::register_hardware_version_callback_v25(std::function<void(std::string&)> callback) { ESP_LOGE(TAG, "non-analog/status information commands not available for slave devices"); }
 void PaceBmsSlave::register_serial_number_callback_v25(std::function<void(std::string&) > callback) { ESP_LOGE(TAG, "non-analog/status information commands not available for slave devices"); }
 void PaceBmsSlave::register_protocols_callback_v25(std::function<void(PaceBmsProtocolV25::Protocols&) > callback) { ESP_LOGE(TAG, "non-analog/status information commands not available for slave devices"); }
