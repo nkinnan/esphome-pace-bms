52250d2aafe4c9903ed3a9b55feb355232f00ed5
Nick Kinnan <nick@nickkinnan.com>
2025-10-13 02:09:30 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -226,9 +226,11 @@ def final_validate_slave_bms_schema(slave_config):
             platform = select_platform.get(CONF_PLATFORM)
             parent_bms_id = select_platform.get(CONF_PACE_BMS_ID)
             if(platform == CONF_PACE_BMS and parent_bms_id == slave_id):
+                print(f"parent_bms_id: {parent_bms_id}")
+                print(f"slave_id: {slave_id}")
                 # we now know that this is the platform for the slave we are validating, now check for invalid components when BMS type is slave
-                #if CONF_PROTOCOL_CAN in select_platform:
-                #    raise cv.Invalid(f"The '{CONF_PROTOCOL_CAN}' select is not available for a BMS with type=SLAVE.")
+                if CONF_PROTOCOL_CAN in select_platform:
+                    raise cv.Invalid(f"The '{CONF_PROTOCOL_CAN}' select is not available for a BMS with type=SLAVE.")
                 if CONF_PROTOCOL_RS485 in select_platform:
                     raise cv.Invalid(f"The '{CONF_PROTOCOL_RS485}' select is not available for a BMS with type=SLAVE.")
                 if CONF_PROTOCOL_TYPE in select_platform:
