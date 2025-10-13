4b76ce4576ebbe0dfda38716c1827c8c5498497b
Nick Kinnan <nick@nickkinnan.com>
2025-10-13 01:39:16 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -20,7 +20,7 @@ from esphome.const import (
 from esphome import pins
 import esphome.final_validate as fv
 
-from .pace_bms_globals import save_pace_bms_schema, CONF_BUTTON
+from .pace_bms_globals import save_pace_bms_schema, CONF_SHUTDOWN
 
 #from .button import CONF_SHUTDOWN
 #import .button
