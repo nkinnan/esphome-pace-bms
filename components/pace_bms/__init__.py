69d1bad5ec340c4666e4d3bc2837f67b688ac55b
Nick Kinnan <nick@nickkinnan.com>
2025-10-15 00:01:31 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -206,6 +206,9 @@ def final_validate_master_bms_schema(master_config):
 
     if(sensor_platforms is not None):
         for sensor_platform in sensor_platforms:
+            
+            print(f"Checking sensor platform: {sensor_platform}")
+
             platform = sensor_platform.get(CONF_PLATFORM)
             parent_bms_id = sensor_platform.get(CONF_PACE_BMS_ID)
             protocol_commandset = master_config.get(CONF_PROTOCOL_COMMANDSET)
