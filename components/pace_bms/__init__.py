7022e0f90e20dc8f45f1aff0ee22c3d2e0589303
Nick Kinnan <nick@nickkinnan.com>
2025-10-14 23:55:38 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -182,13 +182,14 @@ def final_validate_master_bms_schema(master_config):
     full_config = fv.full_config.get()
     master_id = master_config.get(CONF_ID)
 
+    # ensure unique addresses across all pace_bms instances
     address_to_id_name = {}
     pace_bms_configs = full_config.get(CONF_PACE_BMS)
     for pace_bms_config in pace_bms_configs:
         id_name = str(pace_bms_config.get(CONF_ID))
         address = pace_bms_config.get(CONF_ADDRESS)
         if(address_to_id_name.get(address) is not None):
-            raise cv.Invalid(f"Two pace_bms components cannot have the same address ({address}). The two ids are '{id_name}' and '{address_to_id_name[address]}'.")
+            raise cv.Invalid(f"Two pace_bms instances cannot have the same address ({address}). The two ids are '{id_name}' and '{address_to_id_name[address]}'.")
         address_to_id_name[address] = id_name
 
     # the two counts are only available for v25 master
