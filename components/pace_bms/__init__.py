8d5b32f47ddc2460117ebeebedd4c068fdf54218
Nick Kinnan <nick@nickkinnan.com>
2025-10-14 23:57:34 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -188,6 +188,8 @@ def final_validate_master_bms_schema(master_config):
     for pace_bms_config in pace_bms_configs:
         id_name = str(pace_bms_config.get(CONF_ID))
         address = pace_bms_config.get(CONF_ADDRESS)
+        if(address is None):
+            raise cv.Invalid(f"pace_bms instance '{id_name}' does not have an address specified.")
         if(address_to_id_name.get(address) is not None):
             raise cv.Invalid(f"Two pace_bms instances cannot have the same address ({address}). The two ids are '{id_name}' and '{address_to_id_name[address]}'.")
         address_to_id_name[address] = id_name
