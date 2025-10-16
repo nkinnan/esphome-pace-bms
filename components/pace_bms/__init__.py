175fee181b6d7b5d837486b5da1a971777a665a3
Nick Kinnan <nick@nickkinnan.com>
2025-10-15 17:56:58 -0700

    Update __init__.py

--- components/pace_bms/__init__.py
+++ components/pace_bms/__init__.py
@@ -318,9 +318,9 @@ FINAL_VALIDATE_SCHEMA = cv.typed_schema({
     CONF_TYPE_MASTER: cv.All(
         cv.Schema(
         # we can enforce a lot here, but the dump_config() override will output a warning if any of the other settings are not as expected,
-        # and not enforcing them here leaves the door open to weird BMSes with unusual rates even being possible to specify in the yaml at all
-        uart.final_validate_device_schema(CONF_PACE_BMS, baud_rate=9600, require_rx=True, require_tx=True, data_bits=8, parity="NONE", stop_bits=1),
-        #uart.final_validate_device_schema(CONF_PACE_BMS, require_rx=True, require_tx=True),
+        # and not enforcing them here leaves the door open to weird BMSes with unusual rates/etc even being possible to specify in the yaml at all
+        #uart.final_validate_device_schema(CONF_PACE_BMS, baud_rate=9600, require_rx=True, require_tx=True, data_bits=8, parity="NONE", stop_bits=1),
+        uart.final_validate_device_schema(CONF_PACE_BMS, require_rx=True, require_tx=True),
         extra=cv.ALLOW_EXTRA,
         ),
         cv.Schema(
